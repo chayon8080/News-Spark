1a3b3b2e77309fd1a2be55e00f965f0e3ed55fe4
Chayon <chayon15-3633@diu.edu.bd>
2022-09-03 03:26:59 +0600

    fourth commit

--- js/spark.js
+++ js/spark.js
@@ -32,25 +32,24 @@ const displayCatagoriesDetails = (itemDetails) => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card d-flex flex-row">
-                        <div><img src="${detail.thumbnail_url}" class="card-img-top" alt="..."></div>
-                        <div class="card-body">
-                            <div>
-                            <h5 class="card-title">${detail.title}</h5>
-                            <p class="card-text">${detail.details}</p>
-                            </div>
-                            <div>
-                            <div class="d-flex flex-row">
-                            <img src="${detail.thumbnail_url}" class="card-img-top" alt="...">
-                            <h3>${detail.title}</h3>
-                            <button type="button" class="btn btn-primary">See Details</button>
-                            </div>
-                            </div>
-                            
+        <div class="card">
+                       <div class="d-flex flex-row">
+                       <div><img src="${detail.image_url}" class="card-img-top imageAdj py-3" alt="..."></div>
+                       <div class="card-body container">
+                           <div>
+                           <h5 class="card-title">${detail.title}</h5>
+                           <p class="card-text">${detail.details.slice(0, 200)}</p>
+                           </div>
+                           
+                       </div></div>
+                        <div class="d-flex flex-row img-body">
+                        <img  src="${detail.author.img}" class="card-img-top" alt="...">
+                        <p >${detail.author.name}</p>
                         </div>
                     </div>
-                    
+
                     `
+        detailsContainer.appendChild(div);
     })
 }
 
