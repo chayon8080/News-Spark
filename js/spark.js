690f86e2a63ab1b0d3d75c6fea7030aa00668ffc
Chayon <chayon15-3633@diu.edu.bd>
2022-09-03 02:35:41 +0600

    third commit

--- js/spark.js
+++ js/spark.js
@@ -8,26 +8,50 @@ const loadCatagories = async () => {
 
 const displayCatagories = (items) => {
     const catagoriesContainer = document.getElementById('catagories');
-    for (const item of items) {
+    items.forEach(item => {
         const li = document.createElement('li');
         li.classList.add('nav-item')
         li.innerHTML = `
-<a class="nav-link active" onclick="loadItemsData(item.category_id)" aria-current="page" href="#">${item.category_name}</a>
+<a class="nav-link active" onclick="loadItemsData('${item.category_id}')" aria-current="page" href="#">${item.category_name}</a>
 `
         catagoriesContainer.appendChild(li);
-    }
+    })
 }
 
 const loadItemsData = async (itemId) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${itemId}`
     const res = await fetch(url);
     const data = await res.json();
-    displayCatagoriesDetails(data.data.news_category);
+    displayCatagoriesDetails(data.data);
+    console.log(data);
 }
 
-const displayCatagoriesDetails = () => {
+const displayCatagoriesDetails = (itemDetails) => {
     const detailsContainer = document.getElementById('news-container');
-
+    itemDetails.forEach(detail => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card d-flex flex-row">
+                        <div><img src="${detail.thumbnail_url}" class="card-img-top" alt="..."></div>
+                        <div class="card-body">
+                            <div>
+                            <h5 class="card-title">${detail.title}</h5>
+                            <p class="card-text">${detail.details}</p>
+                            </div>
+                            <div>
+                            <div class="d-flex flex-row">
+                            <img src="${detail.thumbnail_url}" class="card-img-top" alt="...">
+                            <h3>${detail.title}</h3>
+                            <button type="button" class="btn btn-primary">See Details</button>
+                            </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                    `
+    })
 }
 
 loadCatagories();
