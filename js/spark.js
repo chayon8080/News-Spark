bd9e57381798785650949d2473ca7c68093fb82d
Chayon <chayon15-3633@diu.edu.bd>
2022-09-04 04:47:49 +0600

    sixth commit

--- js/spark.js
+++ js/spark.js
@@ -12,9 +12,10 @@ const displayCatagories = (items) => {
         const li = document.createElement('li');
         li.classList.add('nav-item')
         li.innerHTML = `
-<a class="nav-link active" onclick="loadItemsData('${item.category_id}')" aria-current="page" href="#">${item.category_name}</a>
+<a id="items" class="nav-link active" onclick="loadItemsData('${item.category_id}')" aria-current="page" href="#"  >${item.category_name}</a>
 `
         catagoriesContainer.appendChild(li);
+
     })
 }
 
@@ -29,6 +30,13 @@ const loadItemsData = async (itemId) => {
 const displayCatagoriesDetails = (itemDetails) => {
     const detailsContainer = document.getElementById('news-container');
     detailsContainer.innerHTML = "";
+    const notFound = document.getElementById('not-found');
+    if (itemDetails.length === 0) {
+        notFound.classList.remove('d-none');
+    }
+    else {
+        notFound.classList.add('d-none');
+    }
     itemDetails.forEach(detail => {
         const div = document.createElement('div');
         div.classList.add('col');
@@ -39,7 +47,7 @@ const displayCatagoriesDetails = (itemDetails) => {
                        <div class="card-body container">
                            <div>
                            <h5 class="card-title">${detail.title ? detail.title : "No Title found"}</h5>
-                           <p class="card-text">${detail.details.slice(0, 200)}</p>
+                           <p class="card-text">${detail.details.slice(0, 200)}...</p>
                            </div>
                            
                        </div></div>
@@ -47,14 +55,46 @@ const displayCatagoriesDetails = (itemDetails) => {
                         <img  src="${detail.author.img ? detail.author.img : "No img found"}" class="card-img-top" alt="...">
                         <div><p >${detail.author.name ? detail.author.name : "No name found"}</p><p >${detail.author.published_date ? detail.author.published_date : "No date found"}</p></div>
                         <div><i class="fa-solid fa-eye"></i> <span class="fw-bold">${detail.total_view ? detail.total_view : "No data found"}</span></div>
-                        <div><i class="fa-solid fa-right-long icon"></i></div>
+                        <div><i onclick="loadModalDetails('${detail.category_id}', '${detail._id}')" class="fa-solid fa-right-long cart-icon icon" data-bs-toggle="modal"  data-bs-target="#blogDetailsModal"></i></div>
                         </div>
                         
                     </div>
 
                     `
         detailsContainer.appendChild(div);
+
     })
+
+    document.getElementById('countData').value = `${itemDetails.length} items found`;
+
 }
+loadItemsData('08', "All News");
+
+const loadModalDetails = async (category_id, id) => {
+    const url = ` https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data, id)
+    displayModalDetails(data.data, id);
+}
+const displayModalDetails = (modals, id) => {
+    const index = modals.indexOf(modals.find(modalItem => modalItem._id === id));
+    console.log(index)
+
+
+    const modalTitle = document.getElementById('blogDetailsModalLabel');
+    modalTitle.innerText = modals[index].title ? modals[index].title : 'Title Not Found';
+    const phoneDetails = document.getElementById('modal-details');
+    phoneDetails.innerHTML = `
+          <img src="${modals[index].author.img}" height="100px" width="100px">
+          <p>Authore Name: ${modals[index].author.name ? modals[index].author.name : 'Author Name Not Found'} </p>
+          <p>Publish Date: ${modals[index].author.published_date ? modals[index].author.published_date : 'Publish Date Not Found'} </p>
+          <p>View: ${modals[index].total_view ? modals[index].total_view : 'Total View Not Found'}</p>
+          <p>Rating: ${modals[index].rating.number ? modals[index].rating.number : 'Rating Not Found'} </p>
+          
+      `;
+}
+
+
 
 loadCatagories();
